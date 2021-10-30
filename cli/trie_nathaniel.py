b8500426746d190e8d5f0d4e36fc5fe501f241df
Nathaniel Thomas <55164518+Nathaniel-github@users.noreply.github.com>
2021-10-30 14:33:25 -0700

    Update docs

--- cli/trie_nathaniel.py
+++ cli/trie_nathaniel.py
@@ -1,11 +1,11 @@
 import socket
-import time
+from struct import pack
 
 HOST = '35.235.69.154'
 PORT = 61135
 
 
-def request(op: str, value: str = None) -> None:
+def request(op: str, value: str = None) -> str:
     """Sends a TCP request to the server with a packet containing the desired operation and value to be added
 
     Args:
@@ -14,10 +14,11 @@ def request(op: str, value: str = None) -> None:
     """
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        s.sendall(bytes(str((op, value)), encoding='utf8'))
+        data = bytes(str((op, value)), encoding='utf8')
+        size = pack('>Q', len(data))
+        s.sendall(size)
+        s.sendall(data)
         data = s.recv(1024).decode('utf8')
 
-    print(data)
-    time.sleep(0.5)
-    print('\n\n')
+    return data
 
